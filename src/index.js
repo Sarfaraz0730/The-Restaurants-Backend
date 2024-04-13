49673ecdfa44eb71f1644e601a25a011148a5d23
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2024-04-13 12:02:42 +0530

    process has beign

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("The Restaurants App")
+const express = require("express")
+const app = express()
+const PORT =8000;
+
+app.get("/",(req,res)=>{
+    res.status(200).send("App setup process begin")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is Running on ${PORT}`)
+})
